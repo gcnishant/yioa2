35e52a6f58a87aebd4166d1a7236b4bd0d9ce415
gcnishant <156033774+gcnishant@users.noreply.github.com>
2024-08-03 10:58:41 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,87 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden,.contact-us,.our-mission,.mentorship-program,.who-we-are,.frequent-questions,.credits');
+hiddenElements.forEach((el) => observer.observe(el))
+
+
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('fixed-header');
+    if (window.scrollY > 50) { // Adjust the scroll value as needed
+        header.classList.add('smaller');
+    } else {
+        header.classList.remove('smaller');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const words = ["empower", "inspire"];
+    let index = 0;
+    const wordElement = document.getElementById('animated-word');
+
+    // Only proceed if wordElement exists
+    if (wordElement) {
+        function changeWord() {
+            wordElement.classList.remove('typing');
+            setTimeout(() => {
+                wordElement.textContent = words[index];
+                wordElement.classList.add('typing');
+                index = (index + 1) % words.length;
+            }, 100);
+        }
+
+        setInterval(changeWord, 3000);
+    } else {
+        console.warn('Element with ID "animated-word" not found');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+    const navLinks = document.querySelectorAll('.nav-menu a');
+
+    // Toggle the navigation menu
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+
+    // Close the navigation menu when a link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const questions = document.querySelectorAll('.question');
+    
+    questions.forEach(question => {
+        question.addEventListener('click', function() {
+            // Close all open answers
+            document.querySelectorAll('.answer').forEach(answer => {
+                answer.style.display = 'none';
+            });
+
+            // Open the clicked answer if it was previously closed
+            const answer = this.nextElementSibling;
+            if (answer.style.display === 'none' || answer.style.display === '') {
+                answer.style.display = 'block';
+            } else {
+                answer.style.display = 'none';
+            }
+        });
+    });
+});
